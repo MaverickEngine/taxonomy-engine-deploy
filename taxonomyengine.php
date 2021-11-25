82091c28d300bf4a26945c92cec4da3cf2f2756d
Jason Norwood-Young <jason@10layer.com>
2021-11-25 14:46:53 +0100

    v0.0.5

--- taxonomyengine.php
+++ taxonomyengine.php
@@ -5,7 +5,7 @@
  * Description: Categorise your WordPress content with the assistance of machine learning and crowdsourcing
  * Author: Daily Maverick, Jason Norwood-Young
  * Author URI: https://dailymaverick.co.za
- * Version: 0.0.4
+ * Version: 0.0.5
  * WC requires at least: 5.8.0
  * Tested up to: 5.8.1
  *
@@ -59,10 +59,3 @@ function taxonomy_engine_automl_init() {
     $taxonomyengine_automl = new TaxonomyEngineAutoML($taxonomyengine_globals);
 }
 add_action( 'init', 'taxonomy_engine_automl_init');
-
-// Shortcodes
-function shortcodes($atts) {
-	// require(plugin_basename("templates/debicheck-form-shortcode.php"));
-}
-
-// add_shortcode( 'debicheck-form', 'shortcodes' );
